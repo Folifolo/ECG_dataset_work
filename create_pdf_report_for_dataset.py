66d7550b138395d03247b82cd15a63a10654f9c9
neuro <nanenaro@gmail.com>
2018-06-15 20:20:58 +0300

    генерация отчета по 2д-датасету

--- create_pdf_report_for_dataset.py
+++ create_pdf_report_for_dataset.py
@@ -3,15 +3,18 @@
 # получаем сюда файл name_pkl с датасетом и генерим для него репорт name_report
 
 import os
+
 from reportlab.lib.pagesizes import letter
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Image
+from reportlab.platypus import SimpleDocTemplate, Paragraph
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib.units import cm
 from reportlab.lib import utils
+import reportlab
 from reportlab.platypus.flowables import HRFlowable
 import pickle as pkl
 import cv2
 import shutil
+import easygui
 
 def create_report(dset_2d_pkl, name_report, labels_names):
 
@@ -20,11 +23,11 @@ def create_report(dset_2d_pkl, name_report, labels_names):
 
     story = [] # сюда будем складывать все сущности, которые должны быть в отчете
     # извлекаем датасет из файла
-    print("loading dataset from file " + dset_2d_pkl)
+    print("загружаем 2д-датасет из файла " + dset_2d_pkl)
     infile = open(dset_2d_pkl, 'rb')
     new_dict = pkl.load(infile)
     infile.close()
-    print("data loaded, len = : " + str(len(new_dict['xs'])))
+    print("загрузили, нашли записей в нем : " + str(len(new_dict['x'])))
 
     # обходим все картинки и добавляем их в отчет (story)
     new_folder = "temp"
@@ -32,12 +35,12 @@ def create_report(dset_2d_pkl, name_report, labels_names):
         shutil.rmtree(new_folder)
     os.makedirs(new_folder)
 
-    for entry_i in range(len(new_dict['xs'])):
+    for entry_i in range(len(new_dict['x'])):
         print (str(entry_i))
-        pic = new_dict['xs'][entry_i]
+        pic = new_dict['x'][entry_i]
         description =""
         for label_name in labels_names:
-            description.append(label_name + " - " + str(new_dict[label_name][entry_i]))
+            description += (label_name + " - " + str(new_dict[label_name][entry_i]))
         img_name = os.path.join(new_folder, str(entry_i)+".png" )
         cv2.imwrite(img_name, pic)
         img = _make_image_for_report(img_name, width=10 * cm)
@@ -65,19 +68,19 @@ def _make_image_for_report(path, width=8*cm):
     img = utils.ImageReader(path)
     iw, ih = img.getSize()
     aspect = ih / float(iw)
-    return Image(path, width=width, height=(width * aspect))
+    return reportlab.platypus.Image(path, width=width, height=(width * aspect))
 
 def _make_line():
     return HRFlowable(width="80%", thickness=1, lineCap='round',  spaceBefore=1, spaceAfter=2, hAlign='CENTER',
                vAlign='BOTTOM', dash=None)
 
 if __name__ == "__main__":
-    name_report = "report.pdf"          # название отчетика
-    dset_name_2d = "2d_healthy.pkl"     # файл с датасетом - 2d
+    name_report = "report_ELECTRIC_AXIS.pdf"          # название отчетика
+    dset_name_2d = easygui.fileopenbox("выберите файл с 2d-датасетом (т.е. с картинками)")
+    labels_names = ['y'] # какие лейблы есть для каждой картинки
 
-    labels_names = ['ys']
-    # labels_names = ['y1', 'y2','y3'] # какие лейблы есть для каждой картинки
-
+    folder_name = 'all_datasets_here'
+    name_report = os.path.join(folder_name, name_report)
     create_report(dset_name_2d, name_report, labels_names)
 
     shutil.rmtree('temp')  # удаляем папку с промежуточным слжебым мусором
--- make_2d_dataset_from_1d.py
+++ make_2d_dataset_from_1d.py
@@ -1,6 +1,77 @@
 # -*- coding: utf-8 -*
-#
-# создать датасет, где экг - двумерные картинки, а не векторы
+
+#  получаем имя файла (dset_1d_pkl) с 1д датасетом, генерим соотвествующий ему
+# 2d датасет и сохраняем его в файл dset_name_2d, параллельно сохраняя
+# картинки в new_folder
+
+import os
+import shutil
+import pickle as pkl
+import matplotlib.pyplot as plt
+import easygui
+import cv2
+
+# https://matplotlib.org/users/image_tutorial.html
+
+
+def gen_2d_dset(dset_1d_pkl, dset_name_2d):
+    # часть 1 - загружаем исходный датасет из файла
+    print("загружаем датасет векторов из файла " + dset_1d_pkl)
+    infile = open(dset_1d_pkl, 'rb')
+    new_dict = pkl.load(infile)
+    infile.close()
+    print("первичный датасет загружен, в нем найдено записей = : " + str(len(new_dict['x'])))
+
+    # часть 2 - генерим картинки
+    x = new_dict['x']
+    temp_folder = 'temp'
+    new_dict['x'] = _generate_pictures(x, temp_folder)
+    shutil.rmtree('temp')  # удаляем папку с промежуточным слжебым мусором
+
+    # часть 3 - сохраняем новый датасет в файл
+    outfile = open(dset_name_2d, 'wb')
+    pkl.dump(new_dict, outfile)
+    outfile.close()
+    print(" сохранеили датасет из картинок в файл: " + dset_name_2d)
+
+
+
+def _generate_pictures(x_1d, temp_folder):
+    if os.path.exists(temp_folder):
+        shutil.rmtree(temp_folder)
+    os.makedirs(temp_folder)
+
+    print(" генерируются картинки...")
+    pics = []
+    id = 0
+    for x in x_1d:
+        pic_name = str(id)+".png"
+        pic_name = os.path.join(temp_folder,pic_name)
+        _save_signal_as_pic(x, pic_name)
+        picture= cv2.imread(pic_name, cv2.COLOR_RGB2GRAY)
+        pics.append(picture)
+        id+=1
+    print("2d imgs created...")
+
+    return pics
+
+def _save_signal_as_pic(x, name):
+    fig = plt.figure()
+    ax = fig.gca()
+
+    ax.plot(x)
+    ax.axis('off')
+    #ax.set_ylim(bottom=ymin, top=ymax) TODO
+    fig.savefig(name)
+    plt.close(fig)
+
 
 if __name__ == "__main__":
-    pass
+    filename_pkl = easygui.fileopenbox()
+    dset_name_2d = "ELECTRIC_AXIS_2d.pkl"
+
+
+    folder_name = 'all_datasets_here'
+    dset_name_2d = os.path.join(folder_name, dset_name_2d)
+
+    gen_2d_dset(filename_pkl, dset_name_2d)
