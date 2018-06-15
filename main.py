bd011ab500ff895b2b9220774ff4a0f824e6bcd1
neuro <nanenaro@gmail.com>
2018-06-15 20:44:44 +0300

    сниппет как открыть готовый датасетик

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 # -*- coding: utf-8 -*
+from pprint import pprint
 
-import create_dataset_n_classes_1d
-import create_dataset_n_len_label
-import make_2d_dataset_from_1d
-import create_pdf_report_for_dataset
-
+import easygui
+import pickle as pkl
+def open_dataset():
+    filename_pkl = easygui.fileopenbox("выберите файл с датасетом")
+    print("загружаем 2д-датасет из файла " + filename_pkl)
+    infile = open(filename_pkl, 'rb')
+    new_dict = pkl.load(infile)
+    infile.close()
+    assert len(new_dict['x']) == len(new_dict['y'])
+    print("загрузили, нашли записей в нем : " + str(len(new_dict['x'])))
+    return new_dict['x'], new_dict['y']
 
 if __name__ == "__main__":
-    pass
+    x, y = open_dataset()
+    pprint(y)
+    pprint(x)
