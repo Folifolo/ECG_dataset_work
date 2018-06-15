f14fa0ccd7611f6d1bbcdf8a83bca9e7c24781ef
neuro <nanenaro@gmail.com>
2018-06-15 19:25:14 +0300

    генерация одномерного датасета

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import create_dataset_n_classes_1d
 import create_dataset_n_len_label
-import create_healthy_unhealthy_dataset
+import make_2d_dataset_from_1d
 import create_pdf_report_for_dataset
 
 
