cd467ce11a2224821c6a1a6b90236912937b2d36
neuro <nanenaro@gmail.com>
2018-07-19 16:58:36 +0300

    аннотации 4 датасета

--- annotator.py
+++ annotator.py
@@ -7,6 +7,7 @@ from tabulate import tabulate
 import matplotlib.pyplot as plt
 import pyedflib
 import numpy as np
+import pickle as pkl
 
 path_to_file = "./delineation.json"
 folder_with_raw_dataset = 'C:\\ecg_new'
@@ -66,6 +67,24 @@ def get_ecg_by_lead_name(ecg_case, lead_name):
         print("какая-то фигня с файлом кардиограммы " + filename_edf)
         return None
 
+def _get_signal_from_file(ecg_case):
+    """
+    :param filename_edf: полное имя .edf файла
+    :return: сигнал с нулевого отведения (т.е. 1 из 12 штук, в форме вектора)
+    """
+    filename_edf = os.path.join(folder_with_raw_dataset, ecg_case + ".edf")
+    try:
+        f = pyedflib.EdfReader(filename_edf)
+        n = f.signals_in_file
+        signals = np.zeros((n, f.getNSamples()[0]))
+        for i in np.arange(n):
+            signals[i, :] = f.readSignal(i)
+        f._close()
+        del f
+        return signals
+    except Exception as  exeption:
+        return None
+
 def print_data(data):
     print(data.keys())  # айдишники пациентов
     print(data['2731'].keys()) # айдишники отведений
@@ -92,6 +111,7 @@ def generate_masks_for_ecg_lead(data, ecg_case, lead_name):
     return masks, tables_names
 
 def test(data):
+    # рисуем маски для первых 5 пациентов для первого отведения - рисунки сохраняем в файлы
     i = 0
     lead_name = leads[0]
     for ecg_case in data.keys():
@@ -105,9 +125,144 @@ def test(data):
         if i>5:
             break
 
+def generate_dataset_argentina(data):
+    #x = 12 отведений ЭКГ, ann = трехслояная маска первого отведения
+    print("начинаем генерацию датасета аннотаций,  x = 12 отведений ЭКГ, ann = трехслояная маска первого отведения")
+    dataset_name = "DSET_argentina.pkl"
+    lead_name = leads[0]
+    x = []
+    ann = []
+    for ecg_case in data.keys():
+        masks, masks_names = generate_masks_for_ecg_lead(data, ecg_case, lead_name)
+        signal = _get_signal_from_file(ecg_case)
+        if signal is None:
+            continue
+        assert len(masks[0]) == len(signal[0]) == MAX
+        x.append(signal)
+        ann.append(np.array(masks))
+    dict = {'x': np.array(x),
+            'ann': np.array(ann),
+            'summary':'3 binary masks'}
+    outfile = open(dataset_name, 'wb')
+    pkl.dump(dict, outfile)
+    outfile.close()
+    print("датасет сохранен в файл " + dataset_name)
+    print("характриситки датасета: инпут = "+ str(dict['x'].shape) + ", аутпут = "+ str(dict['ann'].shape))
+
+def generate_masks_for_ecg_several_leads(data, ecg_case, leads_names):
+    masks = []
+    masks_names = []
+    for lead_name in leads_names:
+        masks_one_lead, masks_names_one_lead = generate_masks_for_ecg_lead(data, ecg_case, lead_name)
+        for mask in masks_one_lead:
+            masks.append(mask)
+        masks_names.append(masks_names_one_lead)
+    return masks, masks_names
+
+
+
+def generate_dataset_jamayka(data):
+    #x = 12 отведений ЭКГ, ann = 12*3 штук бинарных масок
+    print("начинаем генерацию датасета аннотаций,  x = 12 отведений ЭКГ, ann = 12*3 штук бинарных масок")
+    dataset_name = "DSET_jamayka.pkl"
+
+    x = []
+    ann = []
+    for ecg_case in data.keys():
+        masks, masks_names = generate_masks_for_ecg_several_leads(data, ecg_case, leads_names=leads)
+        signal = _get_signal_from_file(ecg_case)
+        if signal is None:
+            continue
+        assert len(masks[0]) == len(signal[0]) == MAX
+        x.append(signal)
+        ann.append(np.array(masks))
+    dict = {'x': np.array(x),
+            'ann': np.array(ann),
+            'summary':'3*12 binary masks'}
+    outfile = open(dataset_name, 'wb')
+    pkl.dump(dict, outfile)
+    outfile.close()
+    print("датасет сохранен в файл " + dataset_name)
+    print("характриситки датасета: инпут = "+ str(dict['x'].shape) + ", аутпут = "+ str(dict['ann'].shape))
+
+def get_mixed_mask(mask1,mask2,mask3):
+    mixed_mask = mask1 + 2*mask2 + 3*mask3
+    return mixed_mask
+
+def generate_mask_for_ecg_lead_NIXED(data, ecg_case, lead_name):
+    masks = []
+    for table_name in tables_names:
+        mask = generate_mask_for_ecg_lead_by_one_table(data,
+                                                       raw_ecg_id=ecg_case,
+                                                       lead_name=lead_name,
+                                                       table_name=table_name)
+        masks.append(mask)
+    masks = np.array(masks)
+    return get_mixed_mask(masks[0], masks[1], masks[2])
+
+def generate_masks_for_ecg_several_leads_MIXED(data, ecg_case, leads_names):
+    masks = []
+    for lead_name in leads_names:
+        maska_one_lead = generate_mask_for_ecg_lead_NIXED(data, ecg_case, lead_name)
+        masks.append(maska_one_lead)
+    return masks
+
+def generate_dataset_urugvay(data):
+    #x = 12 отведений ЭКГ, ann = 1 смешанаая маска первого отведения
+    print("начинаем генерацию датасета аннотаций,  x = 12 отведений ЭКГ, ann = 1 смешанаая маска первого отведения")
+    dataset_name = "DSET_urugvay.pkl"
+    lead_name = leads[0]
+    x = []
+    ann = []
+    for ecg_case in data.keys():
+        maska = generate_mask_for_ecg_lead_NIXED(data, ecg_case, lead_name)
+        signal = _get_signal_from_file(ecg_case)
+        if signal is None:
+            continue
+
+        assert len(maska) == len(signal[0]) == MAX
+        x.append(signal)
+        ann.append(np.array(maska))
+    dict = {'x': np.array(x),
+            'ann': np.array(ann),
+            'summary':'1 not-binary maska'}
+    outfile = open(dataset_name, 'wb')
+    pkl.dump(dict, outfile)
+    outfile.close()
+    print("датасет сохранен в файл " + dataset_name)
+    print("характриситки датасета: инпут = "+ str(dict['x'].shape) + ", аутпут = "+ str(dict['ann'].shape))
+
+def generate_dataset_ostrov_paskhy(data):
+    #x = 12 отведений ЭКГ, ann = 12 небинарных масок (каждому отведению по одной смешаной маске)
+    print("начинаем генерацию датасета аннотаций,  x = 12 отведений ЭКГ, ann = 12 штук НЕбинарных масок")
+    dataset_name = "DSET_ostrov_paskhy.pkl"
+
+    x = []
+    ann = []
+    for ecg_case in data.keys():
+        masks = generate_masks_for_ecg_several_leads_MIXED(data, ecg_case, leads_names=leads)
+        signal = _get_signal_from_file(ecg_case)
+        if signal is None:
+            continue
+        assert len(masks[0]) == len(signal[0]) == MAX
+        x.append(signal)
+        ann.append(np.array(masks))
+    dict = {'x': np.array(x),
+            'ann': np.array(ann),
+            'summary':'3*12 binary masks'}
+    outfile = open(dataset_name, 'wb')
+    pkl.dump(dict, outfile)
+    outfile.close()
+    print("датасет сохранен в файл " + dataset_name)
+    print("характриситки датасета: инпут = "+ str(dict['x'].shape) + ", аутпут = "+ str(dict['ann'].shape))
+
 with open(path_to_file, 'r') as f:
     data = json.load(f)
-    #handle_data(data)
-    test(data)
+    #handle_data(data) распечатать стр-ру джсон - файла с аннотациями
+    #test(data) нарисовать несколько картинок экг с аннотациями
+    #generate_dataset_argentina(data)
+    #generate_dataset_jamayka(data)
+    #generate_dataset_urugvay(data)
+    generate_dataset_ostrov_paskhy(data)
 
 
